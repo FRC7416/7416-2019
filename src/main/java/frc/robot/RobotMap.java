0b497f45df043c4036f4be59ebff949e45675845
Eric <theericnorfleet@gmail.com>
2019-01-24 17:37:14 -0500

    1.1 Switched motor assignments

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,10 +3,13 @@ package frc.robot;
 
 public class RobotMap {
   // drive train
-  public static final int LEFT_FOLLOWER_MOTOR = 0;
+  public static final int RIGHT_MAIN_MOTOR = 0;
+  //public static final int LEFT_FOLLOWER_MOTOR = 0;
   public static final int LEFT_MAIN_MOTOR = 1;
-  public static final int RIGHT_FOLLOWER_MOTOR = 2;
-  public static final int RIGHT_MAIN_MOTOR =3;
+  //public static final int RIGHT_FOLLOWER_MOTOR = 2;
+ 
 
   //Rasberry PI
 }
+
+
